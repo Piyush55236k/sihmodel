0aa6397483d4c2e74a30c343b46a48704ff8cf3f
Piyush Pandey <131640134+Piyush55236k@users.noreply.github.com>
2025-09-02 01:00:34 +0530

    Add files via upload
    add file

--- app.py
+++ app.py
@@ -147,5 +147,7 @@ def recommend():
         return jsonify({"error": str(e)}), 400
 
 # ---------------- Run API ---------------- #
+import os
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
